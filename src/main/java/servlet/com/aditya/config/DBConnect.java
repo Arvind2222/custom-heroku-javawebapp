6d8eb838927e0d680bf24522a8226050667d8cf1
arvind <mishraarvind2222@gmail.com>
2019-09-08 01:25:57 +0530

    intellij

--- src/main/java/servlet/com/aditya/config/DBConnect.java
+++ src/main/java/servlet/com/aditya/config/DBConnect.java
@@ -1,7 +1,5 @@
 package servlet.com.aditya.config;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,8 +8,7 @@ import java.sql.SQLException;
 public class DBConnect {
 
 
-public static Connection getConnect() throws URISyntaxException, SQLException {
-   URI dbUri = new URI(System.getenv("postgres://kagezioqxmdmrd:15bcf3cb86c38c1d9ea086abe50b7d5dfbec2d3f60379e4ea75468802549fe39@ec2-54-235-114-242.compute-1.amazonaws.com:5432/dc1g3kn8qse9je"));
+public static Connection getConnect() throws SQLException {
    String username = "kagezioqxmdmrd";
    String password = "15bcf3cb86c38c1d9ea086abe50b7d5dfbec2d3f60379e4ea75468802549fe39";
    String dbUrl = "jdbc:postgres://ec2-54-235-114-242.compute-1.amazonaws.com:5432/dc1g3kn8qse9je?sslmode=require&user=kagezioqxmdmrd&password=15bcf3cb86c38c1d9ea086abe50b7d5dfbec2d3f60379e4ea75468802549fe39";
