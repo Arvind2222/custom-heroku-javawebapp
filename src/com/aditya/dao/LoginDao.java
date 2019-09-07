b0dcac2a18bc14ce531f55f47a6d54c52e163558
arvind <mishraarvind2222@gmail.com>
2019-09-08 00:05:10 +0530

    First draft

---
+++ src/com/aditya/dao/LoginDao.java
@@ -0,0 +1,35 @@
+package com.aditya.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.aditya.config.DBConnect;
+
+public class LoginDao {
+
+	String sql = "SELECT * FROM USER  where UserName=? and Password=?";
+	String url = "jdbc:mysql://localhost:3306/user";
+	String username = "root";
+	String password = "root";
+
+	public boolean check(String uname, String pass) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DBConnect.getConnect();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, uname);
+			statement.setString(2, pass);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
+
+	}
+
+}
