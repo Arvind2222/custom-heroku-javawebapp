ba386e708b0ebb92fcd0dd97a684677fdefcaac9
arvind <mishraarvind2222@gmail.com>
2019-09-08 00:51:03 +0530

    intellij

--- src/main/java/servlet/com/aditya/dao/LoginDao.java
+++ src/main/java/servlet/com/aditya/dao/LoginDao.java
@@ -9,7 +9,7 @@ import servlet.com.aditya.config.DBConnect;
 public class LoginDao {
 
 	String sql = "SELECT * FROM USER  where UserName=? and Password=?";
-	String url = "postgres://kagezioqxmdmrd:15bcf3cb86c38c1d9ea086abe50b7d5dfbec2d3f60379e4ea75468802549fe39@ec2-54-235-114-242.compute-1.amazonaws.com:5432/dc1g3kn8qse9je";
+	String url = "jdbc:postgres://kagezioqxmdmrd:15bcf3cb86c38c1d9ea086abe50b7d5dfbec2d3f60379e4ea75468802549fe39@ec2-54-235-114-242.compute-1.amazonaws.com:5432/dc1g3kn8qse9je";
 	String username = "kagezioqxmdmrd";
 	String password = "15bcf3cb86c38c1d9ea086abe50b7d5dfbec2d3f60379e4ea75468802549fe39";
 
